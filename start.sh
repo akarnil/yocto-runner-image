d3f2e99e64b9a2b2f591506170a7aaa979e0f2db
akarnil <138698161+akarnil@users.noreply.github.com>
2023-10-20 15:05:29 +0100

    Fix bash if else

--- start.sh
+++ start.sh
@@ -4,12 +4,9 @@ ACCESS_TOKEN=$ACCESS_TOKEN
 USER=$USER
 REPO=$REPO
 
-
 cd /home/docker/actions-runner
 
-# For use with orgs ie avnet-iotconnect
-if [ -z "$ORGANIZATION" ]
-then
+if [[ -z "${ORGANIZATION+set}" ]]; then
     REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
     ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --labels yocto,x64,linux
 else
