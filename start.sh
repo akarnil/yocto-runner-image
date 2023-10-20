8cb4783ee2b91d0797d1671329ea4f0079e9ebb6
Alen Karnil <138698161+akarnil@users.noreply.github.com>
2023-10-20 14:31:02 +0100

    Update start.sh
    Add USER and REPO var

--- start.sh
+++ start.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
+USER=$USER
+REPO=$REPO
 
-# For use with orgs ie avnet-iotconnect
-#REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
-
-# For local repo not in org
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/akarnil/pyoung-yocto-python-sdk/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-# For org
-#./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --labels yocto,x64,linux
-
-# For local repo
-./config.sh --url https://github.com/akarnil/pyoung-yocto-python-sdk --token ${REG_TOKEN} --labels yocto,x64,linux
+# For use with orgs ie avnet-iotconnect
+if [ -z "$ORGANIZATION" ]
+then
+    REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+    ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --labels yocto,x64,linux
+else
+    REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${USER}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+    ./config.sh --url https://github.com/${USER}/${REPO} --token ${REG_TOKEN} --labels yocto,x64,linux
+fi
 
 cleanup() {
     echo "Removing runner..."
